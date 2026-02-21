e0131c8291e0f4cff60e8d7550357fcc51f73028
Shubham Namdeo <shubham.namdeo@hotwax.co>
2026-02-21 12:54:26 +0530

    feat: Set up Playwright testing with HTML reporter and tracing, update the test script, ignore generated files, and remove the default README.

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
+    reporter: 'html',
     use: {
         browserName: 'chromium',
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
     },
 });
